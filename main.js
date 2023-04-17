728e3d19b073f5f6a7b1179b72d91523e35b7c80
sakuisa <sssaku.039@gmail.com>
2023-04-17 19:00:43 +0900

    終了時刻を求め方を追記

--- main.js
+++ main.js
@@ -4,5 +4,11 @@
  const timer = document.getElementById('timer');
  const btn = document.getElementById('btn');
  
+//  終了時刻と求める（開始時に終了時刻を取得したい）
+btn.addEventListener('click' , () => {
+  const endTime = new Date().getTime() + 3 * 1000;
+})
+//  残り時間を求める
+//  タイマーの終了
 
 }
