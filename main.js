7f2542c3c63d84d70c6d9878eefccfcba5bd72c2
sakuisa <sssaku.039@gmail.com>
2023-04-19 19:05:19 +0900

    main.js setIntervalを追記

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-'use strict'
+'use strict';
 
 {
- const timer = document.getElementById('timer');
- const btn = document.getElementById('btn');
- 
-//  終了時刻と求める（開始時に終了時刻を取得したい）
-btn.addEventListener('click' , () => {
-  const endTime = new Date().getTime() + 3 * 1000;
-})
-//  残り時間を求める
-//  タイマーの終了
+  function check() {
+    // 残り時間 = 終了時刻 - 現在時刻
+    const countdown = endTime - new Date().getTime();
 
+    timer.textContent = countdown;
+  }
+
+  const timer = document.getElementById('timer');
+  const btn = document.getElementById('btn');
+  let endTime;
+
+  // (1) 終了時刻を求める
+  btn.addEventListener('click', () => {
+    endTime = new Date().getTime() + 3 * 1000;
+
+    // (2) 残り時間を求める
+    setInterval(check, 100);
+  });
+
+  // (3) タイマーの終了
 }
